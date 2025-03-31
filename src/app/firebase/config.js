67b9b5b286f4eedb55023f519bed5b1811db1390
Guillaume Gay <guillaume.gay@protonmail.com>
2025-03-31 19:13:05 +0200

    dynamic configs

--- src/app/firebase/config.js
+++ src/app/firebase/config.js
@@ -2,14 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBawZLicAm3wnZTYOK0EcHQ9vCqiqI7cmc",
-    authDomain: "train-3328b.firebaseapp.com",
-    databaseURL: "https://train-3328b-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "train-3328b",
-    storageBucket: "train-3328b.firebasestorage.app",
-    messagingSenderId: "393792473153",
-    appId: "1:393792473153:web:cc0da16a3e5d8c26dd5ec0",
-    measurementId: "G-RHG2LYDRM4"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
